f14d2e3a7e0334fba6c1d334c64a83e5809ad4c7
Alexis <alexisrk310@gmail.com>
2023-12-12 11:20:01 -0500

    fix desing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addSchedules } from '../../features/schedules/schedulesSlice';
 import { addSchedulesToday } from '../../features/schedules/schedulesTodaySlice';
 
-const HomePage = ({}) => {
+const HomePage = () => {
 	const dispatch = useDispatch();
 	const schedules = useSelector((state) => state.schedules);
 
@@ -22,34 +22,28 @@ const HomePage = ({}) => {
 			}
 		};
 		init();
-	}, []);
+	}, [dispatch]);
 
-	function calculateAvailableSlots(daySchedule) {
-		// Configuración del horario de atención
+	const calculateAvailableSlots = (daySchedule) => {
 		const startTime = new Date('1970-01-01T09:00:00');
 		const endTime = new Date('1970-01-01T17:00:00');
-		const minDuration = 30 * 60 * 1000; // 30 minutos en milisegundos
-
-		// Crear un conjunto para almacenar los horarios ocupados
+		const minDuration = 30 * 60 * 1000;
 		const occupiedSlots = new Set();
 
-		// Procesar la programación de citas y marcar los horarios ocupados
 		daySchedule?.forEach((entry) => {
 			const entryTime = new Date(`1970-01-01T${entry.Hour}`);
-			const duration = entry?.Duration * 60 * 1000; // Duración en milisegundos
+			const duration = entry?.Duration * 60 * 1000;
 
 			while (entryTime < endTime) {
 				if (entryTime >= startTime && entryTime + duration <= endTime) {
-					// Marcar horario como ocupado
 					occupiedSlots?.add(entryTime?.getTime());
-					entryTime?.setTime(entryTime?.getTime() + minDuration); // Mover al siguiente horario disponible
+					entryTime?.setTime(entryTime?.getTime() + minDuration);
 				} else {
 					break;
 				}
 			}
 		});
 
-		// Calcular el total de espacios disponibles
 		let currentTime = new Date(startTime);
 		let availableSlots = 0;
 
@@ -61,9 +55,8 @@ const HomePage = ({}) => {
 		}
 
 		return availableSlots;
-	}
+	};
 
-	// Obtener el día de la semana actual
 	const currentDay = new Date().getDay();
 	const weekdays = [
 		'domingo',
@@ -75,41 +68,49 @@ const HomePage = ({}) => {
 		'sábado',
 	];
 	const targetDay = weekdays[currentDay];
-	const daySchedule = schedules?.filter((entry) => entry.Day === targetDay);
+
+	const daySchedule = schedules?.filter((entry) => {
+		const entryHour = parseInt(entry.Hour.split(':')[0], 10);
+		const entryDuration = entry.Duration || 0; // Asegúrate de manejar el caso en que Duration no esté definido
+		return (
+			entry.Day === targetDay &&
+			entryHour >= 9 &&
+			entryHour <= 17 &&
+			entryDuration >= 30
+		);
+	});
+
 	useEffect(() => {
 		dispatch(addSchedulesToday(daySchedule));
 	}, [dispatch, schedules, targetDay]);
-	console.log(daySchedule, 'dias filtrados');
+
 	const schedulesToday = useSelector((state) => state.schedulesToday);
-	// Calcular el total de espacios disponibles
 	const totalAvailableSlots = calculateAvailableSlots(daySchedule);
-	console.log(schedulesToday, 'redux filter');
-	console.log(
-		`Total de espacios disponibles para ${targetDay}: ${totalAvailableSlots}`
-	);
-	console.log(schedules, 'state old');
+
 	return (
 		<>
+			<div className="flex justify-center mt-5">
+				<h1 className="text-cyan-700 font-bold text-2xl">Citas disponibles</h1>
+			</div>
+
+			<div className="mt-10 flex justify-center gap-8 flex-wrap">
+				{schedulesToday?.map((schedule, index) => (
+					<CardSchedule
+						Day={schedule.Day}
+						Duration={schedule.Duration}
+						Hour={schedule.Hour}
+						key={index}
+					/>
+				))}
+				<div>
+					Disponibles para el {targetDay}: {totalAvailableSlots}
+				</div>
+			</div>
 			<div className="flex justify-center mt-5">
 				<ModalCreateSchedules
 					openModal={openModal}
 					setOpenModal={setOpenModal}
 				/>
-			</div>
-			<div className="mt-10 flex justify-center gap-8 flex-wrap">
-				{schedulesToday?.map((schedule, index) => {
-					return (
-						<CardSchedule
-							Day={schedule.Day}
-							Duration={schedule.Duration}
-							Hour={schedule.Hour}
-							key={index}
-						/>
-					);
-				})}
-				<div>
-					Disponibles para el {targetDay}: {totalAvailableSlots}
-				</div>
 			</div>
 		</>
 	);
